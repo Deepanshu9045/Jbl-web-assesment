4bea938de0c4451cc6bb64f0734c47964dc0bc2c
Deepanshu9045 <rdeepanshu170@gmail.com>
2026-06-16 21:32:43 +0530

    favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,12 @@ const inter = Inter({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "JBL | Just Put The Headphones On",
   description: "Discover JBL's range of cutting-edge audio solutions.",
+  icons: {
+    icon: "/image.png",
+  },
 };
 
 export default function RootLayout({
@@ -22,7 +25,9 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col font-sans bg-black text-white">{children}</body>
+      <body className="min-h-full flex flex-col font-sans bg-black text-white">
+        {children}
+      </body>
     </html>
   );
 }
